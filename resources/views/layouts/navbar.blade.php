c8ee7ddbbe78068161c1fc8a289ef565cd1be1e2
Darylarz9 <darylariza10@gmail.com>
2026-04-13 14:46:15 -0400

    logo a la derecha

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,22 +6,22 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top shadow">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">
-            @if(optional($config)->logo_path)
-                <img src="{{ Storage::url($config->logo_path) }}" alt="Logo" height="50">
-            @else
-                Fundayacucho
-            @endif
-        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item"><a class="nav-link text-white" href="{{ route('home') }}">Inicio</a></li>
                 <li class="nav-item"><a class="nav-link text-white" href="{{ route('historia') }}">Historia</a></li>
                 <li class="nav-item"><a class="nav-link text-white" href="{{ route('mision') }}">Misión y Visión</a></li>
             </ul>
+            <a class="navbar-brand ms-auto" href="{{ route('home') }}">
+                @if(optional($config)->logo_path)
+                    <img src="{{ Storage::url($config->logo_path) }}" alt="Logo" height="50">
+                @else
+                    Fundayacucho
+                @endif
+            </a>
         </div>
     </div>
 </nav>
